5abd7d77a0e0d73c341e6a876ff937f74c0d8905
“EssamQ97” <jaycem.easy@gmail.com>
2026-01-17 18:24:00 +0300

    refactor: start small, horizontally, then vertically

--- src/domain/meta/i-file-system.ts
+++ src/domain/meta/i-file-system.ts
@@ -1,48 +1,8 @@
-import type { FileSystemFunction } from "./i-file-system-types.js";
+import path from "path";
 
 interface IFileSystem {
-  //File Operations
-  //Read Operations
-  fileRead: FileSystemFunction<string>;
-  fileReadBuffer: FileSystemFunction<Buffer>;
-
-  // Write Operations
-  fileWrite: FileSystemFunction<void>;
-  fileAppend: FileSystemFunction<void>;
-  fileWriteBuffer: FileSystemFunction<void>;
-
-  // Delete Operations
-  fileDelete: FileSystemFunction<void>;
-
-  // Utility Operations
-  fileExists: FileSystemFunction<boolean>;
-  fileCopy: FileSystemFunction<void>;
-  fileMove: FileSystemFunction<void>;
-  fileRename: FileSystemFunction<void>;
-
-  // Directory Operations
-  dirCreate: FileSystemFunction<void>;
-  dirDelete: FileSystemFunction<void>;
-  dirExists: FileSystemFunction<boolean>;
-  dirRead: FileSystemFunction<string[]>;
-  dirCopy: FileSystemFunction<void>;
-
-  // Metadata Operations
-  fileStat: FileSystemFunction<{
-    size: number;
-    createdAt: Date;
-    modifiedAt: Date;
-  }>;
-  fileSize: FileSystemFunction<number>;
-  fileCreatedAt: FileSystemFunction<Date>;
-  fileModifiedAt: FileSystemFunction<Date>;
-  filePermissions: FileSystemFunction<string>;
-
-  // Permission Operations
-  chageFilePermissions: FileSystemFunction<void>;
-  changeDirPermissions: FileSystemFunction<void>;
-  checkFilePermissions: FileSystemFunction<boolean>;
-  checkDirPermissions: FileSystemFunction<boolean>;
+  fileRead: (path: string) => Promise<string>;
+  fileWrite: (path: string, content: string) => Promise<void>;
 }
 
 export type { IFileSystem };
